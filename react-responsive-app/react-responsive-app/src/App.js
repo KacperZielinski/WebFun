891b9a42e622e0347d6ba774df42912e06e5d6ff
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-09-26 13:03:00 +0200

    HomePage component added without CSS part

--- react-responsive-app/react-responsive-app/src/App.js
+++ react-responsive-app/react-responsive-app/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/pages/HomePage/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+      </Switch>
     </Router>
   );
 }
--- react-responsive-app/react-responsive-app/src/components/Section.js
+++ react-responsive-app/react-responsive-app/src/components/Section.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Button } from './Button'
 import { Link } from 'react-router-dom'
 import './Section.css'
-import src from '*.bmp'
-
 
 function Section({
     lightBg,
