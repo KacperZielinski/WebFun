6b218cf801d460bc86859397d88b18ab1c484ce8
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-09-27 16:59:46 +0200

    Parallel JS async/await fun

---
+++ async-await-parallel/api.js
@@ -0,0 +1,38 @@
+const jokeAPI = 'https://official-joke-api.appspot.com/jokes/ten';
+const bitcoinAPI = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+const geoIPAPI = 'https://freegeoip.app/json/';
+
+// es8 (ecma 2017)
+async function parallel() {
+    const jokeRequest = fetch(jokeAPI).then(r => r.json());
+    const bitcoinRequest = fetch(bitcoinAPI).then(r => r.json());
+    const geoIPRequest = fetch(geoIPAPI).then(r => r.json());
+    
+    // those will run in pararel, if you use await on request, they will be executed subsequently
+    const joke = await jokeRequest;
+    const bitcoin = await bitcoinRequest;
+    const geoIP = await geoIPRequest;
+
+    document.getElementById('joke').innerHTML = `${joke[0].setup} ${joke[0].punchline}`; 
+    document.getElementById('bitcoin').innerHTML = `1 bitcoin = ${bitcoin.bpi.USD.rate} $`; 
+    document.getElementById('geoIP').innerHTML = `My ip: ${geoIP.ip}`; 
+    console.log(joke);
+    console.log(bitcoin);
+    console.log(geoIP);
+}
+
+async function sequential(){
+    const joke = await fetch(jokeAPI).then(r => r.json());
+    const bitcoin = await fetch(bitcoinAPI).then(r => r.json());
+    const geoIP = await fetch(geoIPAPI).then(r => r.json());
+
+    document.getElementById('joke_S').innerHTML = `${joke[0].setup} ${joke[0].punchline}`; 
+    document.getElementById('bitcoin_S').innerHTML = `1 bitcoin = ${bitcoin.bpi.USD.rate} $`; 
+    document.getElementById('geoIP_S').innerHTML = `My ip: ${geoIP.ip}`; 
+    console.log(joke);
+    console.log(bitcoin);
+    console.log(geoIP);
+}
+
+parallel();
+sequential();
