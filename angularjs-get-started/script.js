aae7263a8c368fa8ebe366be5c93facb1e6b1e87
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-09-30 15:43:21 +0200

    Added HTTP get request example

--- angularjs-get-started/script.js
+++ angularjs-get-started/script.js
@@ -1,11 +1,22 @@
 (function() {
-
+    const bitcoinAPI = 'https://api.coindesk.com/v1/bpi/currentprice.json';
     var app = angular.module('helloModule', []);
 
-    var HelloController = function($scope) {
+    var HelloController = function($scope, $http) {
+
+        var onBitcoinResult = function(response) {
+            $scope.bitcoin = response.data.bpi.USD.rate;
+        }
+
+        var onError = function(reason) {
+            $scope.bitcoin = "0.0000000001"
+            console.log(reason);
+        }
+
+        $http.get(bitcoinAPI).then(onBitcoinResult, onError);
         $scope.hello = "Hello!";
     }
 
-    app.controller('HelloController', ['$scope', HelloController]);
+    app.controller('HelloController', ['$scope', '$http', HelloController]);
 
 }());
