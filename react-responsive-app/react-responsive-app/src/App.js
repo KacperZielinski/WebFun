c46603d6b6718845b7e4d35b66c32da5c9836b83
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-10-01 21:07:46 +0200

    Added react class component

--- react-responsive-app/react-responsive-app/src/App.js
+++ react-responsive-app/react-responsive-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import ExampleClassComponent from './components/ExampleClassComponent';
 import Home from './components/pages/HomePage/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <ExampleClassComponent />
       <Switch>
         <Route path='/' exact component={Home} />
       </Switch>
---
+++ react-responsive-app/react-responsive-app/src/components/ExampleClassComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ExampleClassComponent extends Component {
+    render() {
+        return (
+            <div className="ecc">Hello from class component!</div>
+        )
+    }
+}
+
+export default ExampleClassComponent;
