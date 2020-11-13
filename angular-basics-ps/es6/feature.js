86270ff61242000a066cb19edc7754db32d75992
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-11-13 21:39:55 +0100

    Shallow copy with spread operator

--- angular-basics-ps/es6/feature.js
+++ angular-basics-ps/es6/feature.js
@@ -2,7 +2,7 @@
 let name = "Foo.."
 console.log(`Hello ${name}`);
 
-// rest operator
+// rest operator, allow pass multiple args to func
 const add = (start, ...num) => {
     let sum = start;
     num.forEach(value => sum += value);
@@ -10,4 +10,37 @@ const add = (start, ...num) => {
 }
 
 console.log(add(0, 1, 2, 3));   // => [1,2,3]
+
+// spread operator, makes shallow copy
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [array1, 6, 7];      // => [[1,2,3,4,5], 6, 7]
+const array3 = [...array1, 6, 7];   // => [1,2,3,4,5,6,7] removes '[]' es6 feature!
+
+const person = {
+    name: 'Stephan',
+    city: 'Wroclaw',
+    details: {
+        id: 52,
+        position: 'worker'
+    }
+}
+
+const superPerson = {
+    ...person,
+    superFlag: true
+}
+
+// shallow copy, primitive value are copied to new place in memory..
+person.name = 'Stephan1'
+superPerson.name = 'Stephan2'
+console.log(person.name)
+console.log(superPerson.name)
+// ..whereas object reference is unchanged.
+console.log(superPerson.details.position)
+console.log(Object.is(person.details, superPerson.details))
+superPerson.details.position = 'super-worker'
+console.log(superPerson.details.position)
+console.log(person.details.position)
+
+
 
