61b5420e7dfcc06d3f3f2c9dcb620a2d223a9e40
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-11-13 22:00:35 +0100

    more es6 features

--- angular-basics-ps/es6/feature.js
+++ angular-basics-ps/es6/feature.js
@@ -1,8 +1,8 @@
-// say hello
-let name = "Foo.."
-console.log(`Hello ${name}`);
+/// say hello from template string
+let fooName = "Foo.."
+console.log(`Hello ${fooName}`);
 
-// rest operator, allow pass multiple args to func
+/// rest operator, allow pass multiple args to func
 const add = (start, ...num) => {
     let sum = start;
     num.forEach(value => sum += value);
@@ -11,7 +11,7 @@ const add = (start, ...num) => {
 
 console.log(add(0, 1, 2, 3));   // => [1,2,3]
 
-// spread operator, makes shallow copy
+/// spread operator, makes shallow copy
 const array1 = [1, 2, 3, 4, 5];
 const array2 = [array1, 6, 7];      // => [[1,2,3,4,5], 6, 7]
 const array3 = [...array1, 6, 7];   // => [1,2,3,4,5,6,7] removes '[]' es6 feature!
@@ -42,5 +42,39 @@ superPerson.details.position = 'super-worker'
 console.log(superPerson.details.position)
 console.log(person.details.position)
 
+/// property shorthand
+const prop1 = 'prop1'
+const prop2 = 'prop2'
 
+// regular
+const p = {
+    prop0: 'prop0',
+    prop1: prop1,
+    prop2: prop2
+}
+
+// newest
+const p_es6 = {
+    prop0: 'prop0',
+    prop1,
+    prop2
+}
+
+/// destrukturyzacja, destructing assignment
+const { name, city } = person;  // create new variables, const name = person.name, const city = person.city
+const { newName, ...other} = person;
+console.log(newName)    // must be exactly the same as obj prop, here it gives undefined!
+console.log(other)
+
+// pretty useful here
+const person1 = {
+    name: '1Stephan',
+    city: '1Wroclaw',
+    details: {
+        id: 152,
+        position: '1worker'
+    }
+}
+[person, person1].map(p => p.name);
+[person, person1].map(({name}) => name);
 
