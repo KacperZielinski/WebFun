66db4c2b09f7cf64005edbfa76c97e402c3e6531
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-11-14 14:48:47 +0100

    ngFor with interpolation

--- angular-basics-ps/ng/sth-with-cars/src/app/app.component.ts
+++ angular-basics-ps/ng/sth-with-cars/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+interface CarCompany {
+  name: string;
+  desc?: string;
+  logoUrl: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sth-with-cars';
+  carCompanies: CarCompany[] = [
+    {
+      name: 'Peugeot',
+      desc: 'Francja, Sochaux, 1810',
+      logoUrl: 'https://www.carlogos.org/car-logos/peugeot-logo.png'
+    },
+    {
+      name: 'Citroen',
+      logoUrl: 'https://www.carlogos.org/car-logos/citroen-logo.png'
+    },
+    {
+      name: 'BMW',
+      logoUrl: 'https://www.carlogos.org/car-logos/bmw-logo.png'
+    },
+    {
+      name: 'Honda',
+      logoUrl: 'https://www.carlogos.org/car-logos/honda-logo.png'
+    },
+    {
+      name: 'Ford',
+      logoUrl: 'https://www.carlogos.org/car-logos/ford-logo.png'
+    },
+
+  ]
 }
