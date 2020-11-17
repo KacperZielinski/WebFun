ff05bdb742dd6772f75fda9938776ca17c1b94ef
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-11-17 12:27:53 +0100

    eager routing added

--- angular-basics-ps/ng/sth-with-cars/src/app/app-routing.module.ts
+++ angular-basics-ps/ng/sth-with-cars/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CarCompanyListComponent} from "./car-company/car-company-list/car-company-list.component";
+import {CarListComponent} from "./car-company/car-list/car-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cars', component: CarListComponent },
+  { path: '', redirectTo: 'car-company', pathMatch: 'full' },
+  { path: '**', component: CarCompanyListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
